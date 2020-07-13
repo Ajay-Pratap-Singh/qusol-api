4cf1a4ae4561b1cfa03285ebade0766cae6d69ca
17aim17 <17aim17@gmail.com>
2020-07-13 18:52:02 +0530

    updated routes/answer.js and added comments

--- routes/answer.js
+++ routes/answer.js
@@ -13,64 +13,47 @@ const errorFormatter = ({ location, msg, param, value, nestedErrors }) => {
 };
 
 
-//for getting all anwsers of a question
-router.get('/question/:id/answer', verifyToken,
-    [
-        param('id', 'No Id in request parameter')
-            .notEmpty()
-            .custom(value => {
-                if (!ObjectId.isValid(value)) return Promise.reject('Not a valid question Id')
-                return true
-            })
-    ],
-    async (req, res) => {
 
-        // error processing
-        const errors = validationResult(req).formatWith(errorFormatter);
 
-        if (!errors.isEmpty()) {
-            return res.status(422).send({
-                error: true,
-                msg: 'could not get answers',
-                body: errors.mapped()
-            });
-        }
-        // error processing ends here
+// ==============================================
+// @GET /question/:id/answers  Getting all answers to a question
+// ==============================================
+router.get('/question/:id/answers', async (req, res) => {
 
-        const { id } = req.params
+    const { id } = req.params
 
-        const page = +req.query.page || 1;
-        const ANSWERS_PER_PAGE = 10;
+    const page = +req.query.page || 1;
+    const ANSWERS_PER_PAGE = 10;
 
-        try {
-            const answers = await Answer.find({ questionId: id, isDeleted: false, isPublished: true }).skip((page - 1) * ANSWERS_PER_PAGE).limit(ANSWERS_PER_PAGE)
+    try {
+        const answers = await Answer.find({ questionId: ObjectId(id), isDeleted: false, isPublished: true }).skip((page - 1) * ANSWERS_PER_PAGE).limit(ANSWERS_PER_PAGE)
 
-            res.status(200).send({
-                error: false,
-                body: {
-                    questionId: id,
-                    page: page,
-                    answers: answers
-                }
-            })
-        } catch (e) {
-            res.status(500).send()
-        }
-    })
-
-//to get one particular answer [authentication not needed]
-router.get('/answer/:answerId', [
-    param('answerId', 'No Id in request parameter')
-        .notEmpty()
-        .custom(value => {
-            if (!ObjectId.isValid(value)) return Promise.reject('Not a valid answer Id')
-            return true
+        res.status(200).send({
+            error: false,
+            body: {
+                questionId: id,
+                page: page,
+                answers: answers
+            }
         })
-], async (req, res) => {
+    } catch (e) {
+        res.status(500).send()
+    }
+})
+
+
+
+
+
+
+// ==============================================
+// @GET /answer/:answerId  to get one particular answer [authentication not needed]
+// ==============================================
+router.get('/answer/:answerId', async (req, res) => {
     const _id = req.params.answerId
 
     try {
-        const ans = await Answer.findOne({ _id, isDeleted: false, isPublished: true })
+        const ans = await Answer.findOne({ _id: ObjectId(_id), isDeleted: false, isPublished: true })
         if (!ans) {
             return res.status(404).send()
         }
@@ -80,7 +63,66 @@ router.get('/answer/:answerId', [
     }
 })
 
-//to post an answer to a question [authentication needed]
+
+
+
+
+
+// ==============================================
+// @GET /question/:questionId/answer   get draft of a user for a particular question
+// ==============================================
+router.get('/question/:id/answer', verifyToken, async (req, res) => {
+
+    const { id } = req.params
+
+    try {
+        const ans = await Answer.findOne({ questionId: ObjectId(id), author: req.user })
+        if (!ans) {
+            return res.status(404).send()
+        }
+        res.status(200).send({ error: false, body: ans })
+    } catch (e) {
+        res.status(500).send()
+    }
+
+})
+
+
+
+
+
+
+// ==============================================
+// @put /question/:questionId/answer/draft  To make draft of an answer
+// ==============================================
+router.put('/question/:id/answer/draft', verifyToken, [
+    check('body', 'answer cannot be empty').not().isEmpty()
+], async (req, res) => {
+
+    const { body, isPublished = false } = req.body
+
+    try {
+        await Answer.findOneAndUpdate(
+            { questionId: ObjectId(req.params.id), author: req.user },
+            { body, isPublished },
+            { upsert: true }
+        )
+        res.status(200).send({ msg: saved })
+    } catch (e) {
+        console.log(e);
+        res.status(500).send()
+    }
+
+})
+
+
+
+
+
+
+// ==============================================
+// @POST /answer  to post an answer to a question [authentication needed]
+// ==============================================
 router.post('/answer', verifyToken, [
     check('questionId', 'No question id provided')
         .notEmpty()
@@ -104,20 +146,15 @@ router.post('/answer', verifyToken, [
         });
     }
     // error processing ends here
-    const { body, isAnonymous = false, questionId, isPublished = false } = req.body
+    const { body, isAnonymous = false, questionId, isPublished = true } = req.body
 
-    const ansObject = {
+    const ans = new Answer({
         questionId,
         body,
-        author: req.user
-    }
-
-    if (isPublished) {
-        ansObject.isAnonymous = isAnonymous
-        ansObject.isPublished = isPublished
-    }
-
-    const ans = new Answer(ansObject)
+        author: req.user,
+        isAnonymous,
+        isPublished
+    })
 
     try {
         await ans.save()
@@ -140,67 +177,14 @@ router.post('/answer', verifyToken, [
     }
 })
 
-// ============================
-// draft routes
-// ============================
 
-// @GET /answer/draft get draft of a user for a particular question
-router.get('/answer/draft', verifyToken, async (req, res) => {
 
-    const { questionId = null } = req.body
 
-    try {
-        const ans = await Answer.findOne({ questionId: ObjectId(questionId), author: req.user, isPublished: false })
-        if (!ans) {
-            return res.status(404).send()
-        }
-        res.status(200).send({ error: false, body: ans })
-    } catch (e) {
-        res.status(500).send()
-    }
 
-})
 
-router.patch('/answer/draft', verifyToken, [
-    check('questionId', 'No question id provided')
-        .notEmpty()
-        .custom(value => {
-            if (!ObjectId.isValid(value)) return Promise.reject('Not a valid question Id')
-            return true
-        }),
-    check('body', 'answer cannot be empty').not().isEmpty()
-
-], async (req, res) => {
-
-    // error processing
-
-    const errors = validationResult(req).formatWith(errorFormatter);
-
-    if (!errors.isEmpty()) {
-        return res.status(422).send({
-            error: true,
-            msg: 'could not save answer',
-            body: errors.mapped()
-        });
-    }
-    // error processing ends here
-
-    const { body, questionId } = req.body
-    try {
-        await Answer.findOneAndUpdate(
-            { questionId: ObjectId(questionId), author: req.user, isPublished: false },
-            { body },
-            { upsert: true }
-        )
-        res.status(200).send({ msg: saved })
-    } catch (e) {
-        console.log(e);
-        res.status(500).send()
-    }
-
-})
-
-//to edit an answer to a question [authentication needed]
+// ==============================================
+// @PATCH /answer/:id  to edit an answer to a question [authentication needed]
+// ==============================================
 router.patch('/answer/:id', verifyToken, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['body', 'isAnonymous']
@@ -225,7 +209,14 @@ router.patch('/answer/:id', verifyToken, async (req, res) => {
     }
 })
 
-//to delete an answer to a question [authentication needed]
+
+
+
+
+
+// ==============================================
+// @DELETE /answer/:id  to delete an answer to a question [authentication needed]
+// ==============================================
 router.delete('/answer/:id', verifyToken, async (req, res) => {
     try {
         const ans = await Answer.findOneAndUpdate({ _id: req.params.id, author: req.user, isDeleted: false }, { isDeleted: true })
